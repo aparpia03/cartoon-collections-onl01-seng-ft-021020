e58a44a64e8036defa0bf8aefbea537fc2461886
Abbas Parpia <aparpia03@gmail.com>
2020-02-15 18:08:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,6 +23,9 @@ end
 def find_the_cheese(array_of_strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array_of_strings.include?("cheddar")
-    
+  array_of_strings = []
+  cheese_types.collect do |cheese|
+    array_of_strings.push(cheese)
+    array_of_strings.include?("cheddar")
+  end 
  end 
