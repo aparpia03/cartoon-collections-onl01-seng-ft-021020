60f7da34e14818c424bec89a8a8c0ad3c4549395
Abbas Parpia <aparpia03@gmail.com>
2020-02-15 16:50:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def roll_call_dwarves(names)# code an argument here
  end
 end
 
-def summon_captain_planet(calls)# code an argument here
+=begin def summon_captain_planet(calls)# code an argument here
   # Your code here
   calls.map do |call|
     call.split()
