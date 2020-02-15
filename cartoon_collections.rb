59177bbcfdbc5c92448952a524e98698b788c18c
Abbas Parpia <aparpia03@gmail.com>
2020-02-15 18:16:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,6 +23,6 @@ end
 def find_the_cheese(array_of_strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-array_of_strings.detect 
+array_of_strings.detect{|cheese|
     
  end 
